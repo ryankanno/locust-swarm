31383353f88e0b2f239e8a173200ceba117e0a46
Ryan Kanno <ryankanno@localkinegrinds.com>
2013-10-11 15:02:37 -0400

    Updated swarm.

--- locust-swarm/swarm.py
+++ locust-swarm/swarm.py
@@ -4,8 +4,10 @@
 from boto.ec2 import connect_to_region
 from config import config
 from fabric.api import env
+from fabric.api import put
+from fabric.api import roles
+from fabric.api import run
 from fabric.api import sudo
-from fabric.api import task
 from fabric.tasks import execute
 from multiprocessing import Pool
 from time import sleep
@@ -24,7 +26,17 @@ FABRIC_ENV_KEY_FILENAME = config.get('fabric', 'key_filename', None)
 pool = Pool(processes=4)
 
 
-def _run_instances(ami_id, tag_dict):
+def _get_instances(filters):
+    
+    conn = connect_to_region(
+        AWS_REGION,
+        aws_access_key_id=AMI_ACCESS_KEY_ID,
+        aws_secret_access_key=AMI_SECRET_ACCESS_KEY)
+
+    return conn.get_all_instances(filters=filters)
+
+
+def _run_instances(ami_id, tag_dict, security_groups):
     
     conn = connect_to_region(
         AWS_REGION,
@@ -66,35 +78,59 @@ def create_slave(ami_id, name='Locust-Slave'):
     return _run_instances(ami_id, {'Name': name})
 
 
+def get_masters(name='Locust-Master'):
+    return _get_instances({'tag:Name': name})
+
+
+def get_slaves(name='Locust-Slave'):
+    return _get_instances({'tag:Name': name})
+
+
 @roles('master')
-def start_master():
-    pass
+def bootstrap_master():
+    _bootstrap("master")
+    run('locust -f /tmp/locust/bootstrap-master/locustfile.py --master &', pty=False) 
 
 
 @roles('slave')
-def start_slave():
-    pass
+def bootstrap_slave(master_ip_address):
+    _bootstrap("slave")
+    run("locust -f /tmp/locust/bootstrap-slave/locustfile.py --slave --master-host {0} &".format(master_ip_address), pty=False)
 
-    
-@task
-def update():
-    sudo('apt-get update && apt-get upgrade -y')
-    sudo('apt install python') 
-    sudo('apt install python-pip python-dev build-essential')
-    sudo('pip install --upgrade pip')
-    put('./sample-locust.py', '/tmp/sample-locust.py')
+def _bootstrap(role):
+    run('mkdir -p /tmp/locust/')
+    put("/Users/ryankanno/Projects/github/me/locust-swarm/example/bootstrap-{0}/".format(role), '/tmp/locust/')
+    sudo("chmod +x /tmp/locust/bootstrap-{0}/bootstrap.sh".format(role))
+    sudo("/tmp/locust/bootstrap-{0}/bootstrap.sh".format(role))
+
+
+def update_role_defs(reservations, role_key):
+    env.roledefs[role_key] = []
+    for reservation in reservations:
+        for instance in reservation.instances:
+            if instance.ip_address:
+                env.roledefs[role_key].append(instance.ip_address)
+
 
 if __name__ == '__main__':
-    master = create_master(AMI_ID)
+    #create_master(AMI_ID)
 
     # TODO: parallelize and parameterize 
-    slave1 = create_slave(AMI_ID)
-    slave2 = create_slave(AMI_ID)
+    #create_slave(AMI_ID)
+    #create_slave(AMI_ID)
+    #create_slave(AMI_ID)
+    #create_slave(AMI_ID)
 
-    env.hosts = [master.ip_address, slave1.ip_address, slave2.ip_address]
+    masters = get_masters()
+    slaves = get_slaves()
+
+    update_role_defs(masters, 'master')
+    update_role_defs(slaves, 'slave')
+
     env.user = FABRIC_ENV_USER
     env.key_filename = FABRIC_ENV_KEY_FILENAME
-    execute(update)
 
+    execute(bootstrap_master)
+    execute(bootstrap_slave)
 
 # vim: filetype=python
